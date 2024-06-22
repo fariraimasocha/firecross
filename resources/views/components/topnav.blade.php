fb7fa989c6dcf37b6243931e400bfe6026948414
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-06-22 20:48:09 +0200

    add car images

--- resources/views/components/topnav.blade.php
+++ resources/views/components/topnav.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-gradient-to-r from-blue-50 to-red-400 h-20">
+<nav class="h-20">
     <div class="container mx-auto flex items-center justify-between h-full">
         <h1 class="font-light text-4xl">
             <span class="font-bold text-blue-950">Fire</span>cross
@@ -10,7 +10,7 @@
             <a href="/">Analytics</a>
         </div>
         <a href="/">
-            <button class="bg-gradient-to-r from-blue-950 to-red-400 px-4 py-2 rounded-lg">Join for 0$</button>
+            <button class="bg-blue-950 px-4 py-2 rounded-lg text-white">Join for 0$</button>
         </a>
     </div>
 </nav>
