efa7648d3799ed799a3d5673457aab3cb60999eb
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-06-22 22:18:20 +0200

    work on the home card and car

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,15 @@
 @extends('layout.app')
 
 @section('content')
-    <div class="container mx-auto mt-10">
-        <h1 class="text-3xl font-bold mb-6">Ready to find your deal.</h1>
-        <p>Simply click the "Find your deal now" button to start browsing our car inventory.
-            Your perfect ride awaits</p>
+    <div class="container mx-auto relative">
+        <img src="{{ asset('images/cbmw.png') }}" alt="No-Image" width="100%" class="mt-4">
+        <div class="absolute top-0 left-0 w-9/12 h-full flex py-32 px-40">
+            <div class="bg-white p-4">
+                <h1 class="text-3xl font-bold mb-6">Ready to find your deal.</h1>
+                <p>Simply click the "Find your deal now" button to start browsing our car inventory.
+                    Your perfect ride awaits</p>
+            </div>
+        </div>
+
     </div>
 @endsection
