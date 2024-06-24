0f50bb5039b14e76e6038e9f8a4c6af8b50542e4
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-06-24 13:59:41 +0200

    feat restructure

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,7 @@
 @extends('layout.app')
 
 @section('content')
-    <div class="container mx-auto relative">
-        <img src="{{ asset('images/cbmw.png') }}" alt="No-Image" width="100%" class="mt-4">
-        <div class="absolute top-0 left-0 w-9/12 h-full flex py-32 px-40">
-            <div class="bg-white p-4">
-
-            </div>
-        </div>
-
+    <div class="">
 
     </div>
 @endsection
