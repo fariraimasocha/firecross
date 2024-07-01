47cd432a0906e2432313c6caf4c50e3b99fee008
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-07-01 17:28:37 +0200

    wip
    hover on icons

--- resources/views/components/topnav.blade.php
+++ resources/views/components/topnav.blade.php
@@ -1,7 +1,7 @@
 <nav class="h-20">
     <div class="container mx-auto flex items-center justify-between h-full">
         <div class="flex space-x-3">
-            <a href="#" class="inline-block py-4 md:py-0">
+            <a href="# hover:text-xl transition" class="inline-block py-4 md:py-0">
                 <span class="p-1 text-xl font-black leading-none text-gray-900">firecross.</span>
             </a>
         </div>
@@ -14,15 +14,15 @@
         </div>
         <div class="flex space-x-5">
             <a href="/">
-                <i class="text-gray-500 hover:text-gray-700 fa-brands fa-twitter text-lg"></i>
+                <i class="text-gray-500 hover:text-gray-700 fa-brands fa-twitter text-lg hover:text-xl transition"></i>
             </a>
             <a href="/">
-                <i class="text-gray-500 hover:text-gray-700 fa-brands fa-facebook text-lg"></i>
+                <i class="text-gray-500 hover:text-gray-700 fa-brands fa-facebook text-lg hover:text-xl transition"></i>
             </a>
             <a href="/">
-                <i class="text-gray-500 hover:text-gray-700 fa-solid fa-globe text-lg"></i></a>
+                <i class="text-gray-500 hover:text-gray-700 fa-solid fa-globe text-lg hover:text-xl transition"></i></a>
             <a href="/">
-                <i class="text-gray-500 hover:text-gray-700 fa-brands fa-instagram text-lg"></i>
+                <i class="text-gray-500 hover:text-gray-700 fa-brands fa-instagram text-lg hover:text-xl transition"></i>
             </a>
         </div>
     </div>
