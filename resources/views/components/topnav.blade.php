7e81f2d75c61f6dbdd165c688d6d069983b0f52f
Farirai Masocha <114150009+fariraimasocha@users.noreply.github.com>
2024-07-05 16:27:50 +0200

    wip
    add search icon

--- resources/views/components/topnav.blade.php
+++ resources/views/components/topnav.blade.php
@@ -2,7 +2,7 @@
     <div class="container mx-auto flex items-center justify-between h-full">
         <div class="flex space-x-3">
             <a href="#" class="inline-block py-4 md:py-0">
-                <span class="p-1 text-xl font-black leading-none text-gray-900">firecross.</span>
+                <span class="p-1 text-xl font-black leading-none text-gray-900">Firecross.</span>
             </a>
         </div>
 
@@ -11,6 +11,8 @@
             <a href="/" class="hover:font-bold hover:text-gray-900 transition">Discover</a>
             <a href="/" class="hover:font-bold hover:text-gray-900 transition">Shop</a>
             <a href="{{route('drive.index')}}" class="hover:font-bold hover:text-gray-900 transition">Drive</a>
+
+
         </div>
         <div class="flex space-x-5">
             <a href="/">
@@ -23,6 +25,9 @@
                 <i class="text-gray-500 hover:text-gray-700 fa-solid fa-globe text-lg hover:text-xl transition"></i></a>
             <a href="/">
                 <i class="text-gray-500 hover:text-gray-700 fa-brands fa-instagram text-lg hover:text-xl transition"></i>
+            </a>
+            <a href="/">
+                <i class="text-gray-500 hover:text-gray-700 fa fa-search text-lg hover:text-xl transition"></i>
             </a>
         </div>
     </div>
